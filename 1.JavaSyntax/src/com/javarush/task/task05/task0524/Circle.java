b8e8a077925bca1e99a6c435223d0a974aaff947
bezumka <Gbhfvblf87>
2017-03-15 17:57:24 +0200

    initial commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0524/Circle.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task05.task0524;
+
+/* 
+Основа колеса
+В классе Circle создать конструктор который проинициализирует все переменные класса.
+*/
+
+public class Circle {
+    public double x;
+    public double y;
+    public double r;
+
+    public Circle(double x, double y, double r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0606/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task06.task0606;
+
+import java.io.*;
+
+/* 
+Чётные и нечётные циферки
+Ввести с клавиатуры число. Определить, сколько в введенном числе четных цифр, а сколько нечетных.
+Если число делится без остатка на 2 (т. е. остаток равен нулю), значит оно четное.
+Увеличиваем на 1 счетчик четных цифр (статическая переменная even).
+Иначе число нечетное, увеличиваем счетчик нечетных цифр (статическая переменная odd).
+Вывести на экран сообщение: «Even: а Odd: b», где а — количество четных цифр, b — количество нечетных цифр.
+*/
+
+public class Solution {
+
+    public static int even;
+    public static int odd;
+
+    public static void main(String[] args) throws IOException {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = reader.readLine();
+        Integer integer;
+        for (int i = 0; i < line.length(); i++) {
+            integer = Integer.parseInt(String.valueOf(line.charAt(i)));
+            if ((integer % 2) == 0) {
+                even++;
+            }else if ((integer % 2) != 0){
+                odd++;
+            }
+        }
+        System.out.println("Even: " + even + " Odd: " + odd);
+        reader.close();
+    }
+}
